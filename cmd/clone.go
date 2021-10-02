c58ff92db525464c5a3e37fca348732b0e0e1ef5
OleDakotaJoe <stevendakotasheaves@gmail.com>
2021-10-02 03:35:06 -0500

    renamed to common

--- cmd/clone.go
+++ cmd/clone.go
@@ -1,6 +1,5 @@
 package cmd
 
-
 import (
 	"clonr/utils"
 	"fmt"
@@ -18,7 +17,8 @@ var cloneCmd = &cobra.Command{
 	Long:  `All software has versions. This is Clonr'`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Running Clonr Project Templating CLI --- v0.0.1")
-		source := args[0]
+		var source = args[0]
+		fmt.Printf("%s \n", source)
 		validateUrl(source)
 		cloneProject(source, outputDir)
 
@@ -27,12 +27,12 @@ var cloneCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(cloneCmd)
-	cloneCmd.Flags().StringVar(&outputDir, "out", "./clonr-app", "The git URL to read from")
+	cloneCmd.Flags().StringVar(&outputDir, "name", "clonr-app", "The git URL to read from")
 }
 
 
 func validateUrl(source string) {
-	fmt.Println("Validating src URL")
+	fmt.Println("Validating Source URL")
 	_,err := url.ParseRequestURI(source)
 	utils.CheckForError(err)
 	fmt.Println("The src URL you provided is valid.")
@@ -40,7 +40,8 @@ func validateUrl(source string) {
 
 func cloneProject(source string, outputDir string) {
 	fmt.Println("Cloning git repo... Please Wait")
-	_, err := git.PlainClone(outputDir, false,&git.CloneOptions{
+
+	_, err := git.PlainClone(outputDir, false, &git.CloneOptions{
 		URL: source,
 		Progress: os.Stdout,
 	} )
--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"clonr/cmd"
+	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 func main() {
 	cmd.Execute()
+}
+
+func init() {
+	// Set up Logging
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.WarnLevel)
 }
