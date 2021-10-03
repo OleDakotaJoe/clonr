f4154559868206a29e33f247dba4b87f8f1f11e1
OleDakotaJoe <stevendakotasheaves@gmail.com>
2021-10-03 15:03:53 -0500

    Finished replacement engine

--- core/file_parser.go
+++ core/file_parser.go
@@ -6,6 +6,7 @@ import (
 	"clonr/utils"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cast"
 	"github.com/spf13/viper"
 	"io/ioutil"
 	"os"
@@ -19,31 +20,36 @@ func ProcessFiles(configFilePath string) {
 	log.Debugf("Paths: %s", paths)
 	for path := range paths {
 		log.Infof("Processing path: %s", path)
-		filePathKey := configRootKey +"."+ path
-		pathData := v.GetStringMapString(filePathKey)
+		pathData := cast.ToStringMap(paths[path])
+		fileLocation := configFilePath + cast.ToString(pathData[config.GlobalConfig().TemplateFileLocationKeyName])
+		variableKey := configRootKey + "." + path + "." + config.GlobalConfig().VariablesArrayKeyName
+		variables := v.GetStringMap(variableKey)
+		processedVarMap := make(map[string]string)
+
 		log.Debugf("Raw pathData: %s", pathData)
-		fileLocation := pathData[config.GlobalConfig().TemplateFileLocationKeyName]
-		variableArrayKey := filePathKey + "." + config.GlobalConfig().VariablesArrayKeyName
-		variablesMap := v.Get(variableArrayKey)
-		//processedVarMap := make(map[string]string)
-		log.Debug(fileLocation, variablesMap)
-		//for question, variable := range variablesMap {
-		//	processedVarMap[variable] = answerQuestion(fmt.Sprintf("%s", question))
-		//}
-		//log.Debugf("Processed varMap: %s", processedVarMap)
+		log.Debugf("Processing file at location: %s", fileLocation)
+		log.Debugf("Variables: %s", variables)
+
+
+		for variable, _ := range variables {
+			questionKey := variableKey + "." + variable
+			question := v.GetStringMapString(questionKey)["question"]
+			processedVarMap[variable] = answerQuestion(question)
+			log.Debugf("variable: %s, question: %s", variable, question)
+		}
+
 		//// Renders the file below
-		//log.Infof("Rendering file: %s, with vars: %s", path, variablesMap)
-		//RenderFile(fileLocation, variablesMap)
+		log.Infof("Rendering file: %s, with vars: %s", path, processedVarMap)
+		RenderFile(fileLocation, processedVarMap)
 	}
 }
-
 func RenderFile(filename string, varMap map[string]string) {
 	input, err := ioutil.ReadFile(filename)
 	utils.CheckForError(err)
 	inputFileAsString := string(input)
 	for key, value := range varMap {
 		pattern := config.GlobalConfig().ClonrPrefix + key + config.GlobalConfig().ClonrSuffix
-		log.Debugf("Line: %s, Pattern: %s, value: %s, key: %s", inputFileAsString, pattern, value, key)
+		log.Debugf("InputFileAsString: %s, Pattern: %s, value: %s, key: %s", inputFileAsString, pattern, value, key)
 		inputFileAsString= strings.Replace(inputFileAsString, pattern, value , 1)
 		log.Debugf("Rendering Variable: %s", key)
 	}
@@ -57,9 +63,7 @@ func ViperReadConfig(configFilePath string) *viper.Viper {
 	v.AddConfigPath(configFilePath)
 	log.Debugf("Config File Location: %s", v.ConfigFileUsed())
 	err := v.ReadInConfig()
-	log.Debug("Running")
 	utils.CheckForError(err)
-	log.Debug("Running 2")
 	return v
 }
 
