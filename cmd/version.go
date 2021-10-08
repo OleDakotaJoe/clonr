d5ad4ff8eeba1cc665c1c511329ab6c0fc59ee49
OleDakotaJoe <stevendakotasheaves@gmail.com>
2021-10-08 18:29:12 -0500

    feat: brew support

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Clonr",
 	Long:  `All software has versions. This is Clonr's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.1.7")
+		fmt.Println("v0.1.8")
 		fmt.Println("Clonr CLI -- Project Templating Engine")
 	},
 }
