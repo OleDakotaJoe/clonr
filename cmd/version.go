6a8fa3431728117a98ec36015ee261606b1d20dc
OleDakotaJoe <stevendakotasheaves@gmail.com>
2021-10-08 16:36:31 -0500

    First Release

--- cmd/version.go
+++ cmd/version.go
@@ -10,8 +10,8 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Clonr",
 	Long:  `All software has versions. This is Clonr's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.0.1")
-		fmt.Println("Clonr CLI -- Project Templating Engines")
+		fmt.Println("v0.1.0")
+		fmt.Println("Clonr CLI -- Project Templating Engine")
 	},
 }
 
