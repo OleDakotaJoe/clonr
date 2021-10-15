b565751cbc197c8e224585e6aaefdc882c506134
Steven Sheaves <stevendakotasheaves@gmail.com>
2021-10-15 03:14:34 -0500

    refactor: refactored locations of tests

--- core/process_files_test.go
+++ core/process_files_test.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"github.com/oledakotajoe/clonr/config"
+	"github.com/oledakotajoe/clonr/types"
 	log "github.com/sirupsen/logrus"
 	"testing"
 )
@@ -14,20 +15,20 @@ func Test_setup(t *testing.T) {
 func Test_givenTemplateFile_getFileMapFromTemplate(t *testing.T) {
 	sourceDir := config.TestConfig().SourceDir
 
-	testSettings := &FileProcessorSettings{
+	testSettings := &types.FileProcessorSettings{
 		ConfigFilePath: sourceDir,
-		Reader: func(input string) string {
+		StringInputReader: func(input string) string {
 			return input
 		},
 	}
 
-	getFileMapFromConfigFile(testSettings)
-	exampleFileMap := FileMap{
-		"../testing-resources/process_files_test/source_template/sub-dir/another-test.txt": ClonrVarMap{"file_sub_dir_multi_diff_1": "file_sub_dir_multi_diff_1", "file_sub_dir_multi_diff_2": "file_sub_dir_multi_diff_2"},
-		"../testing-resources/process_files_test/source_template/test.txt":                 ClonrVarMap{"file_in_root_multi": "file_in_root_multi"},
+	processClonrConfig(testSettings)
+	exampleFileMap := types.FileMap{
+		"../.testing-.resources/process_files_test/source_template/sub-dir/another-test.txt": types.ClonrVarMap{"file_sub_dir_multi_diff_1": "file_sub_dir_multi_diff_1", "file_sub_dir_multi_diff_2": "file_sub_dir_multi_diff_2"},
+		"../.testing-.resources/process_files_test/source_template/test.txt":                 types.ClonrVarMap{"file_in_root_multi": "file_in_root_multi"},
 	}
 
-	for key, value := range testSettings.mainTemplateMap {
+	for key, value := range testSettings.MainTemplateMap {
 		log.Infof("key: %s, value: %s", key, value)
 		if exampleFileMap[key] == nil {
 			t.Fatalf("Maps were not equivalent")
