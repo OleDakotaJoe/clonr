9ae1a682651509829c84a46e1f124526fb8380eb
Steven Sheaves <stevendakotasheaves@gmail.com>
2021-10-15 03:08:54 -0500

    refactor: cleaned up and restructured project

--- config/test_config.go
+++ config/test_config.go
@@ -8,9 +8,9 @@ type testConfig struct {
 
 func TestConfig() *testConfig {
 	this := testConfig{
-		SourceDir:    "../testing-resources/process_files_test/source_template",
-		OutputDir:    "../testing-resources/process_files_test/test_output",
-		AnswerKeyDir: "../testing-resources/process_files_test/answer_key",
+		SourceDir:    "../.test-project/process_files_test/source_template",
+		OutputDir:    "../.test-project/process_files_test/test_output",
+		AnswerKeyDir: "../.test-project/process_files_test/answer_key",
 	}
 	return &this
 }
