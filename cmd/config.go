47eefd5b886b601e4758f8b52dae70c8e79f14b0
Steven Sheaves <stevendakotasheaves@gmail.com>
2021-10-17 01:13:58 -0500

    feat: added functionality to set configuration with a command clonr config set

--- cmd/config.go
+++ cmd/config.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/oledakotajoe/clonr/config"
 	"github.com/oledakotajoe/clonr/utils"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
 	"os"
 	"reflect"
+	"strings"
 	"text/tabwriter"
 )
 
@@ -27,6 +29,7 @@ func init() {
 	// Sub-commands
 	configCmd.AddCommand(showCmd)
 	configCmd.AddCommand(setCmd)
+	configCmd.AddCommand(resetCmd)
 }
 
 var showCmd = &cobra.Command{
@@ -41,8 +44,10 @@ var showCmd = &cobra.Command{
 			writer := tabwriter.NewWriter(os.Stdout, 28, 8, 1, '\t', tabwriter.AlignRight)
 			field := typeConf.Field(i).Name
 			value := cast.ToString(v.Field(i))
-			_, pErr := fmt.Fprintf(writer, "%s:\t %s \n", field, value)
-			utils.ExitIfError(pErr)
+			if field != "Viper" {
+				_, pErr := fmt.Fprintf(writer, "%s:\t %s \n", field, value)
+				utils.ExitIfError(pErr)
+			}
 			wErr := writer.Flush()
 			utils.ExitIfError(wErr)
 		}
@@ -54,13 +59,69 @@ var setCmd = &cobra.Command{
 	Short: "Make an adjustment to clonr's configuration",
 	Long:  `Display clonr's current configuration.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		propertyName := args[0]
+		value := args[1]
 		v := config.Global().Viper
-		err := v.WriteConfig()
-		if os.IsNotExist(err) {
-			err := v.WriteConfigAs(utils.GetLocationOfInstalledBinary())
-			fmt.Println(utils.GetLocationOfInstalledBinary())
-			utils.ExitIfError(err)
+		switch propertyName {
+		case "DefaultProjectName":
+			v.Set("DefaultProjectName", value)
+			break
+		case "ConfigFileName":
+			v.Set("ConfigFileName", value)
+			break
+		case "ConfigFileType":
+			v.Set("ConfigFileType", value)
+			break
+		case "PlaceholderRegex":
+			v.Set("PlaceholderRegex", value)
+			break
+		case "PlaceholderPrefix":
+			v.Set("PlaceholderPrefix", value)
+			break
+		case "PlaceholderSuffix":
+			v.Set("PlaceholderSuffix", value)
+			break
+		case "TemplateRootKeyName":
+			v.Set("TemplateRootKeyName", value)
+			break
+		case "TemplateLocationKeyName":
+			v.Set("TemplateLocationKeyName", value)
+			break
+		case "VariablesKeyName":
+			v.Set("VariablesKeyName", value)
+			break
+		case "GlobalsKeyName":
+			v.Set("GlobalsKeyName", value)
+			break
+		case "QuestionsKeyName":
+			v.Set("QuestionsKeyName", value)
+			break
+		case "DefaultAnswerKeyName":
+			v.Set("DefaultAnswerKeyName", value)
+			break
+		case "DefaultChoicesKeyName":
+			v.Set("DefaultChoicesKeyName", value)
+			break
+		case "LogLevel":
+			v.Set("LogLevel", value)
+			break
+		default:
+			fmt.Println()
+			log.Errorf("%s is not a clonr property.", propertyName)
+			fmt.Printf("\nRun 'clonr config show' for a list of options. No changes were made\n")
 		}
+		utils.SaveConfig(v, utils.GetLocationOfInstalledBinary()+config.Global().ConfigFileName)
 	},
 }
-
+var resetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Display clonr's current configuration.",
+	Long:  `Display clonr's current configuration.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		response := utils.StringInputReader("Are you sure you what to do this? (y/n)")
+		if strings.ToLower(response) != "y" {
+			return
+		}
+		config.ResetGlobalToDefault()
+	},
+}
